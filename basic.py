a911456b70e0f45f53a3357a234073ad3d07b94c
unknown <mr.neozora@gmail.com>
2019-08-24 21:51:45 +0800

    Update basic.py

--- basic.py
+++ basic.py
@@ -2,12 +2,6 @@ import requests
 import ffmpy
 import time
 
-source = ""  # Insert stream url
-file = ""  # Insert target media file
-chunk_size = 1024  # bytes
-resp_timeout = 5  # secs
-delay = 0.5  # secs
-
 headers = {
     'user-agent':
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
@@ -16,44 +10,46 @@ headers = {
 }
 
 
-def extract(source, file):
+# extraction
+
+source_url = "<Insert stream url here>"
+file = "<Insert target media file here>"
+chunk_size = 1024 # bytes
+resp_timeout = 5 # secs
+
+
+def extract(source_url, file):
     with open(file, "ab") as f:
-        resp = requests.get(source, stream=True, headers=headers, timeout=resp_timeout)
+        resp = requests.get(source_url, stream=True, timeout=resp_timeout)
         for chunk in resp.iter_content(chunk_size):
             f.write(chunk)
-        f.close()
 
 
-# extract(source, file)
+# filtration
 
-playlist = ""
-path = ""
+delay = 0.5 # secs
+playlist_url = "<Insert playlist url here>"
 
-def filter(playlist, path):
-    resp = requests.get(playlist, headers=headers, timeout=resp_timeout)
+
+def filter(playlist_url):
+    resp = requests.get(playlist_url, timeout=resp_timeout)
     content = resp.content.decode("utf-8").splitlines()
     for line in content:
         if line[0] != "#":
-            source = path + line
-            extract(source, file)
+            extract(line, file)
             time.sleep(delay)
 
 
-# filter(playlist, path)
+# conversion / multiplex
 
-video_file = ""  # Insert video file here
-audio_file = ""  # Insert audio file here
-output_file = ""  # Insert output media file name here
+video_file = "<Insert video file here>"
+audio_file = "<Insert audio file here>"
+output_file = "<Insert output media file name here>"
 
 
 def mux(video_file, audio_file, output_file):
     Ff = ffmpy.FFmpeg(
-        inputs={video_file: None, audio_file: None},
-        outputs={output_file: "-b:v 1M"}
+            inputs = {video_file: None, audio_file: None},
+            outputs = {output_file: "-b:v 1M" }
     )
     ff.run()
-
-
-# mux(video_file, audio_file, output_file)
-
-print("Process finished.")
